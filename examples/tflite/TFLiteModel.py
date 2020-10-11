aee1527e06b8e2e34c27b5690205ea8eee9c2cb2
Adrian Gonzalez-Martin <agm@seldon.io>
2020-10-11 11:17:53 +0100

    Use seldon-core instead of mlserver

---
+++ examples/tflite/TFLiteModel.py
@@ -0,0 +1,21 @@
+from seldon_core.user_model import SeldonComponent
+from tflite_runtime import interpreter as tflite
+
+
+class TFLiteModel(SeldonComponent):
+    def __init__(self, model_uri="./models/face_mask_detection.tflite"):
+        self._model_uri = model_uri
+
+    def load(self):
+        self._interpreter = tflite.Interpreter(model_path=self._model_uri)
+        self._interpreter.allocate_tensors()
+
+    def predict(self, X):
+        # NOTE: This is not thread-safe!
+        self._interpreter.set_tensor("input0", X)
+        self._interpreter.invoke()
+
+        output_name = "output0"
+        output = self._interpreter.get_tensor(output_name)
+
+        return output
