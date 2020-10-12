9142d09fa0004d63f6be1c07cbbed28f9e899db7
Adrian Gonzalez-Martin <agm@seldon.io>
2020-10-12 17:16:16 +0100

    Add Dockerfile for camera-reader

--- examples/face-mask-detector/camera-reader/camera.py
+++ examples/face-mask-detector/camera-reader/camera.py
@@ -1,3 +1,4 @@
+import logging
 import requests
 import time
 import numpy as np
@@ -8,36 +9,44 @@ from picamera.array import PiRGBArray
 from typing import Tuple
 
 
-DEBUG = True
+DEBUG = False
 PIXEL_FORMAT = "RGB"
 FACEMASK_DETECTOR_SERVER = "http://model:9000"
 
 
 def _get_camera(resolution: Tuple[int, int] = (640, 480)) -> PiCamera:
+    logging.info(f"Accessing camera with {resolution} resolution")
     camera = PiCamera()
-    camera.resolution = (640, 480)
+    camera.resolution = resolution
     # Start a preview and let the camera warm up for 2 seconds
+    logging.info("Waiting for camera to warm up...")
     camera.start_preview()
     time.sleep(2)
 
+    logging.info("Obtained camera handle!")
     return camera
 
 
 def _save_frame(frame: np.ndarray):
+    frame_file_path = "last-image.png"
+    logging.debug(f"Saving frame in {frame_file_path}...")
     img = Image.fromarray(frame, PIXEL_FORMAT)
-    img.save("last-image.png")
+    img.save(frame_file_path)
 
 
 def _run_inference(frame: np.ndarray) -> np.ndarray:
+    logging.debug(f"Running inference in frame with shape {frame.shape}...")
     batch = np.expand_dims(frame, axis=0)
     payload = {"data": {"ndarray": batch.tolist()}}
     endpoint = f"{FACEMASK_DETECTOR_SERVER}/api/v1.0/predictions"
 
+    logging.debug(f"Sending request to inference endpoint {endpoint}...")
     response = requests.post(endpoint, json=payload)
     if not response.ok:
         raise RuntimeError("Invalid frame")
 
     y_pred = response.json()
+    logging.debug(f"Obtained prediction with shape {y_pred.shape}")
 
     # TODO: Filter out lower than threshold and keep classes
 
@@ -45,6 +54,7 @@ def _run_inference(frame: np.ndarray) -> np.ndarray:
 
 
 def _update_leds():
+    logging.debug("Updating LEDs...")
     pass
 
 
